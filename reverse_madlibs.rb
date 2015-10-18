cdedae0ba828cc9a676542ac4166e842b8245bfc
Rich-McGrath <richmcgrath@gmail.com>
2015-10-17 22:36:03 -0400

    Refactored No Input file if statement

--- reverse_madlibs.rb
+++ reverse_madlibs.rb
@@ -2,10 +2,8 @@ def say(msg)
   puts "=> #{msg}"
 end
 
-if ARGV.empty?
-  say 'No input file!'
-  exit
-end
+say 'No input file!' && exit if ARGV.empty?
+  
 
 if !File.exists?(ARGV[0]) #Check to see if input file exists
   say "Input file dosen't exist!"
