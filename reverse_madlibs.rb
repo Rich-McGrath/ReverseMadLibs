86f67816d564439f00ab8810065e7c9ffbd9b3ac
Rich-McGrath <richmcgrath@gmail.com>
2015-10-18 19:40:26 -0400

    Asking user for noun Input

--- reverse_madlibs.rb
+++ reverse_madlibs.rb
@@ -25,6 +25,11 @@ def exit_with(msg)
   exit
 end
 
+def get_inputs(word)
+  say "Input a #{word}"
+  STDIN.gets.chomp
+end
+
 exit_with('No input file!') && exit if ARGV.empty?
 exit_with("Input file dosen't exist!") && exit if !File.exist?(ARGV[0])
 
@@ -32,8 +37,9 @@ contents = File.open(ARGV[0], 'r') do |f|
   f.read
 end
 
-contents.gsub!('NOUN').each do
-  dictionary[:nouns].sample
+contents.gsub!('NOUN').each do |noun|
+  # dictionary[:nouns].sample
+  get_inputs(noun)
 end
 
 contents.gsub!('VERB').each do
