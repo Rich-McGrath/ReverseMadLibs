f7f616690ea527b7915d4919587ea9a4ae4d5aea
Rich-McGrath <richmcgrath@gmail.com>
2015-10-18 17:10:12 -0400

    Created Dictionary and gsub

--- reverse_madlibs.rb
+++ reverse_madlibs.rb
@@ -1,3 +1,9 @@
+dictionary = {
+  nouns: ['dog', 'car', 'clown', 'hat'],
+  verbs: ['juggle', 'spin'],
+  adjectives: ['giant', 'red'],
+}
+
 def say(msg)
   puts "=> #{msg}"
 end
@@ -9,3 +15,10 @@ end
 
 exit_with('No input file!') && exit if ARGV.empty?
 exit_with("Input file dosen't exist!") && exit if !File.exist?(ARGV[0])
+
+contents = File.open(ARGV[0], 'r') do |f|
+  f.read
+end
+
+contents.gsub('NOUN', dictionary[:nouns].sample)
+p contents
