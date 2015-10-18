e48f2a88734e71911fb6f5376775ee660cfc9047
Rich-McGrath <richmcgrath@gmail.com>
2015-10-17 22:37:56 -0400

    Refactore inputl file dosen't exist if statement

--- reverse_madlibs.rb
+++ reverse_madlibs.rb
@@ -3,9 +3,4 @@ def say(msg)
 end
 
 say 'No input file!' && exit if ARGV.empty?
-  
-
-if !File.exists?(ARGV[0]) #Check to see if input file exists
-  say "Input file dosen't exist!"
-  exit
-end
+say "Inout file dosen't exist!" && exit if !File.exists?(ARGV[0]) #Check to see if input file exists
