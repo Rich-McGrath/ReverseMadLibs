23726f7ddc89ea6dcd47714ec29a33f3068b8f80
Rich-McGrath <richmcgrath@gmail.com>
2015-10-17 22:47:40 -0400

    Fixed If statements

--- reverse_madlibs.rb
+++ reverse_madlibs.rb
@@ -2,5 +2,10 @@ def say(msg)
   puts "=> #{msg}"
 end
 
-say 'No input file!' && exit if ARGV.empty?
-say "Inout file dosen't exist!" && exit if !File.exists?(ARGV[0]) #Check to see if input file exists
+def exit_with(msg)
+  say msg
+  exit
+end
+
+exit_with('No input file!') && exit if ARGV.empty?
+exit_with("Input file dosen't exist!") && exit if !File.exist?(ARGV[0])
